aea5d10a68a9885b46456dc869c317962ebeed1a
fargutvest@gmail.com <fargutvest@gmail.com>
2020-01-17 21:37:20 +0300

    dynamic table fish

--- src/App.js
+++ src/App.js
@@ -3,34 +3,45 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const characters = [
+      {
+        English: "Hello",
+        Spelling: "spelling =)",
+        Russian: "Privet"
+      },
+      {
+        English: "Hello",
+        Spelling: "spelling =)",
+        Russian: "Privet"
+      }
+    ] 
     return (
   <div>
-	<Row/>
-	<Row/>
-	<Row/>
-  <Row/>
+	<TableBody characterData={characters}/>
   </div>
     );
   }
 }
 
-class Row extends Component {
-  render() {
+
+const TableBody = props => {
+const rows = props.characterData.map((row, index) => {
     return (
-  <tr>
-    <td><Sticker/></td><td><Sticker/></td><td><Sticker/></td><td><Sticker/></td>
+  <tr key={index}>
+    <td><Sticker data={row}/></td>
   </tr>
-    );
-  }
+    )
+  })
+  return <tbody>{rows}</tbody>
 }
 
 class Sticker extends Component {
   render() {
     return (
       <div className="Sticker">
-          <p>English word</p>
-	        <p>Spelling</p>
-	        <p>Russian word</p>
+          <p>{this.props.data.English}</p>
+	        <p>{this.props.data.Spelling}</p>
+	        <p>{this.props.data.Russian}</p>
       </div>
     );
   }
