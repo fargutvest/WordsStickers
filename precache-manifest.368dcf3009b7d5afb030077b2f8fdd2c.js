98fb0cd020c2e3e00aee2ea2ecd545b23d049f6f
Henadzi Myslitski <henadzi.myslitski@capsilon.com>
2020-01-20 15:36:25 +0300

    Updates

---
+++ precache-manifest.368dcf3009b7d5afb030077b2f8fdd2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc5bd34bb224b37e4ba6e1633e25454",
+    "url": "/EnglishWordsStickersJS/index.html"
+  },
+  {
+    "revision": "31f4cc53a5c3393163bb",
+    "url": "/EnglishWordsStickersJS/static/css/main.403b71cb.chunk.css"
+  },
+  {
+    "revision": "9f80d9d7e2c376883b8c",
+    "url": "/EnglishWordsStickersJS/static/js/2.b16cc1e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/EnglishWordsStickersJS/static/js/2.b16cc1e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31f4cc53a5c3393163bb",
+    "url": "/EnglishWordsStickersJS/static/js/main.7bc6ce9d.chunk.js"
+  },
+  {
+    "revision": "99b7f049f323ebbbf4c0",
+    "url": "/EnglishWordsStickersJS/static/js/runtime-main.b0faf887.js"
+  }
+]);
