38fe9e53152588d7a1204dc6dbdaf2a62b98b963
Henadzi Myslitski <fargutvest@gmail.com>
2020-04-18 20:51:52 +0000

    Updates

--- precache-manifest.9082d4a11572a6264d581cdaa80bbf38.js
+++ precache-manifest.9082d4a11572a6264d581cdaa80bbf38.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7ff9ab1a971d57854a0e88896b2f0d54",
+    "revision": "87279f19086749566d711a3d681e7839",
     "url": "/EnglishWordsStickers/index.html"
   },
   {
-    "revision": "60200bdfe234e364033f",
-    "url": "/EnglishWordsStickers/static/css/main.e536cf09.chunk.css"
+    "revision": "10a5649c15022f3c1f5e",
+    "url": "/EnglishWordsStickers/static/css/main.b9a35e06.chunk.css"
   },
   {
-    "revision": "a9e2a9b8c72ab4250b76",
-    "url": "/EnglishWordsStickers/static/js/2.911cdd9e.chunk.js"
+    "revision": "dc943e5903351cd7e4f8",
+    "url": "/EnglishWordsStickers/static/js/2.72092fc8.chunk.js"
   },
   {
     "revision": "79326bab6d30e35dc37dcd6e4c9b7bd8",
-    "url": "/EnglishWordsStickers/static/js/2.911cdd9e.chunk.js.LICENSE.txt"
+    "url": "/EnglishWordsStickers/static/js/2.72092fc8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "60200bdfe234e364033f",
-    "url": "/EnglishWordsStickers/static/js/main.9cbbf6f8.chunk.js"
+    "revision": "10a5649c15022f3c1f5e",
+    "url": "/EnglishWordsStickers/static/js/main.0b0f668d.chunk.js"
   },
   {
     "revision": "cf735e29d36b3132dfe7",
@@ -32,12 +32,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/EnglishWordsStickers/static/media/google.7c9224eb.svg"
   },
   {
-    "revision": "9817adf83e8085acdb751244f95e13e7",
-    "url": "/EnglishWordsStickers/static/media/pdf-book.9817adf8.svg"
+    "revision": "4abb284eacfc00cfe56e46d70422e545",
+    "url": "/EnglishWordsStickers/static/media/pdf-book.4abb284e.svg"
   },
   {
-    "revision": "6c3daf6085e46eea1698990ad3578119",
-    "url": "/EnglishWordsStickers/static/media/preloader.6c3daf60.svg"
+    "revision": "cc338b03daa4ddb77607d0b8b27fcb9b",
+    "url": "/EnglishWordsStickers/static/media/preloader.cc338b03.svg"
   },
   {
     "revision": "496292e15ac64ebe146cd8c511a80b7b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/EnglishWordsStickers/precache-manifest.1d2177a917b77f0d834c82a1f9b52e68.js"
+  "/EnglishWordsStickers/precache-manifest.9082d4a11572a6264d581cdaa80bbf38.js"
 );
 
 self.addEventListener('message', (event) => {
